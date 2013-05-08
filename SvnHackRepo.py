ec7e3a591b5c22af6f5b80dd2ced70168d294d46
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2013-05-07 23:35:06 -0600

    Remove SvnHack in favor of switching to an __init__.py

--- SvnHackRepo.py
+++ SvnHackRepo.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-class SvnHackRepo:
+class Repo:
     """The SvnHack representation of a repository."""
     def __init__(self, name):
         self.name = name
 
 class SvnRepo(Repo):
-    """A representation of a remote Subversion repository."""
+    """The SvnHack representation of a remote Subversion repository."""
     def __init__(self, name):
-        
+        Repo.__init__(self, name)
