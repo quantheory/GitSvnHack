aef14278c824d8068c0b8971c23fe182ca6f3eca
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2013-05-10 20:28:52 -0600

    Add read_repo method to ConfigFile.

--- ConfigFile.py
+++ ConfigFile.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 
-from SvnHack.RepoClasses import GitSvnRepo
+from SvnHack.RepoClasses import SvnRepo, GitSvnRepo
+import re
+
+# Regex for empty lines in a file (i.e. only whitespace or comments).
+empty_line = re.compile("^\s*(#.*)$")
+
+# Regex for configuration line in a file.
+# Currently does not allow end-of-line comments.
+config_line = re.compile("^\s*(?P<var>\w+)\s*=\s*(?P<value>.*\S)\s*$")
 
 class ConfigFile():
     """Class for files defining the Subversion to Git translation."""
     def __init__(self, file_path):
         self.path = file_path
+    def read_repo(self):
+        config_values = dict()
+        with open(self.path, "r") as my_file:
+            for line in my_file:
+                if empty_line.match(line):
+                    continue
+                re_match = config_line.match(line)
+                if re_match:
+                    re_match.group("var")
+                    config_values[re_match.group("var")] = \
+                        re_match.group("value")
+                else:
+                    # Unrecognized line, so exit.
+                    return
+        svn_repo = SvnRepo( "svn_"+config_values["name"],
+                            config_values["svnurl"])
+        repo = GitSvnRepo( config_values["name"],
+                           config_values["path"],
+                           svn_repo )
+        return repo
--- testConfigFile.py
+++ testConfigFile.py
@@ -10,6 +10,12 @@ class TestConfigFile(unittest.TestCase):
         self.my_config_file = ConfigFile.ConfigFile(self.file_path)
     def test_path(self):
         self.assertEqual(self.my_config_file.path, self.file_path)
+    def test_read_repo(self):
+        repo = self.my_config_file.read_repo()
+        self.assertEqual(repo.name, "test_repo")
+        self.assertEqual(repo.path, "/path/to/test_repo")
+        self.assertEqual(repo.svn_repo.name, "svn_test_repo")
+        self.assertEqual(repo.svn_repo.path, "https://path/to/svn_origin")
 
 
 if __name__ == "__main__":
