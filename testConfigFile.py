86cf9d1e47100f8b4ba98bf22b62400d44af5252
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2013-05-10 22:53:50 -0600

    Write out test config files during test rather than keeping them in the repo.

--- testConfigFile.py
+++ testConfigFile.py
@@ -2,38 +2,64 @@
 
 from SvnHack import ConfigFile
 import unittest
+from tempfile import mkstemp
+import os
 
-class TestConfigFile(unittest.TestCase):
-    """Test the "ConfigFile" class."""
-    cfg_def = "test_files/test_config.def"
+def write_temp_file(string):
+    """Writes a string to a temporary file and returns the name."""
+    fd,cfg_name = mkstemp(text=True)
+    with os.fdopen(fd, "w") as tmp_file:
+        tmp_file.write(string)
+    return cfg_name
+
+class TestConfigBase(unittest.TestCase):
+    """Base test class for ConfigFile tests."""
+    cfg_file_class = ConfigFile.ConfigFile
     def setUp(self):
-        self.my_cfg_file = ConfigFile.ConfigFile(self.cfg_def)
+        """Create a mock configuration file."""
+        self.cfg_name = write_temp_file(self.file_string)
+        self.my_cfg_file = self.cfg_file_class(self.cfg_name)
+    def tearDown(self):
+        """Remove configuration file."""
+        os.remove(self.cfg_name)
+
+class TestConfigFile(TestConfigBase):
+    """Test the "ConfigFile" class."""
+    key_str = "key1"
+    val_str = "key2"
+    file_string = key_str+"="+val_str
     def test_path(self):
-        self.assertEqual(self.my_cfg_file.path(), self.cfg_def)
+        """Test "path" method output."""
+        self.assertEqual(self.my_cfg_file.path(), self.cfg_name)
     def test_read_dict(self):
+        """Test "read_dict" output."""
         cfg_dict = self.my_cfg_file.read_dict()
-        test_dict = { "key1": "value1" }
+        test_dict = { self.key_str: self.val_str }
         self.assertDictEqual(cfg_dict, test_dict)
 
-class TestConfigFileErr(unittest.TestCase):
-    """Test errors from the "ConfigFile" class."""
+class TestConfigFileBadLine(TestConfigBase):
+    """Test bad line error from the "ConfigFile" class."""
+    file_string = "This is no good as a ConfigFile."
     def test_bad_line(self):
-        cfg_def = "test_files/test_config_broken.def"
-        my_cfg_file = ConfigFile.ConfigFile(cfg_def)
+        """Test that a file with a bad line raises ValueError."""
         with self.assertRaises(ValueError):
-            my_cfg_file.read_dict()
+            self.my_cfg_file.read_dict()
 
-class TestGitSvnDefFile(unittest.TestCase):
+class TestGitSvnDefFile(TestConfigBase):
     """Test the "TestGitSvnDefFile" class."""
-    repo_def = "test_files/test_repo.def"
-    def setUp(self):
-        self.my_repo_file = ConfigFile.GitSvnDefFile(self.repo_def)
+    cfg_file_class = ConfigFile.GitSvnDefFile
+    repo_name = "test_repo"
+    repo_path = "/path/to/test_repo"
+    repo_svnurl = "https://path/to/svn_origin"
+    file_string = "name = "+repo_name+"\n" \
+        "path = "+repo_path+"\n" \
+        "svnurl = "+repo_svnurl
     def test_read_repo(self):
-        repo = self.my_repo_file.read_repo()
-        self.assertEqual(repo.name, "test_repo")
-        self.assertEqual(repo.path, "/path/to/test_repo")
-        self.assertEqual(repo.svn_repo.name, "svn_test_repo")
-        self.assertEqual(repo.svn_repo.path, "https://path/to/svn_origin")
+        repo = self.my_cfg_file.read_repo()
+        self.assertEqual(repo.name, self.repo_name)
+        self.assertEqual(repo.path, self.repo_path)
+        self.assertEqual(repo.svn_repo.name, "svn_"+self.repo_name)
+        self.assertEqual(repo.svn_repo.path, self.repo_svnurl)
 
 if __name__ == "__main__":
     unittest.main()
