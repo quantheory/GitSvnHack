1b3a6182145eb881c0465c0161b64afb2d14d7eb
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2013-05-20 23:03:36 -0600

    Remove the ConfigFile class that's superseded by the standard library's ConfigParser.

--- ConfigFile.py
+++ ConfigFile.py
@@ -4,41 +4,6 @@
 from GitSvnHack.RepoClasses import SvnBranch, SvnRepo, GitSvnRepo
 
 from configparser import ConfigParser, ExtendedInterpolation
-import re
-
-# Regex for empty lines in a file (i.e. only whitespace or comments).
-empty_line = re.compile("^\s*(#.*)?$")
-
-# Regex for empty lines in a file (i.e. only whitespace or comments).
-section_line = re.compile("^\s*\[[^]]+\]$")
-
-# Regex for configuration line in a file.
-# Currently does not allow end-of-line comments.
-config_line = re.compile("^\s*(?P<var>\w+)\s*=\s*(?P<value>.*\S)\s*$")
-
-class ConfigFile:
-    """Class for general-purpose configuration files."""
-    def __init__(self, path):
-        self._path = path
-    def get_path(self):
-        """Return path that this object was created with."""
-        return self._path
-    def get_dict(self):
-        """Read configuration file into a dictionary."""
-        config_vars = dict()
-        with open(self._path, "r") as my_file:
-            for line in my_file:
-                if empty_line.match(line) or section_line.match(line):
-                    continue
-                re_match = config_line.match(line)
-                if re_match:
-                    config_vars[re_match.group("var")] = \
-                        re_match.group("value")
-                else:
-                    # Unrecognized line, so raise exception.
-                    raise ValueError( \
-                        "Unparseable line in config file.")
-        return config_vars
 
 class GitSvnDefFile:
     """Class for files defining the Subversion to Git translation."""
--- testConfigFile.py
+++ testConfigFile.py
@@ -15,7 +15,6 @@ def write_temp_file(string):
 
 class TestConfigBase(unittest.TestCase):
     """Base test class for ConfigFile tests."""
-    cfg_file_class = ConfigFile.ConfigFile
     def setUp(self):
         """Create a mock configuration file."""
         self.cfg_name = write_temp_file(self.file_string)
@@ -23,43 +22,6 @@ class TestConfigBase(unittest.TestCase):
     def tearDown(self):
         """Remove configuration file."""
         os.remove(self.cfg_name)
-
-class TestConfigFile(TestConfigBase):
-    """Test the "ConfigFile" class."""
-    sect_str = "section1"
-    key_str = "key1"
-    val_str = "value1"
-    file_string = "["+sect_str+"]\n"+key_str+"="+val_str
-    def test_path(self):
-        """Test "path" method output."""
-        self.assertEqual(self.my_cfg_file.get_path(), self.cfg_name)
-    def test_get_dict(self):
-        """Test "get_dict" output."""
-        cfg_dict = self.my_cfg_file.get_dict()
-        test_dict = { self.key_str: self.val_str }
-        self.assertDictEqual(cfg_dict, test_dict)
-
-class TestConfigFileBadLine(TestConfigBase):
-    """Test bad line error from the "ConfigFile" class."""
-    file_string = "This is no good as a ConfigFile."
-    def test_bad_line(self):
-        """Test that a file with a bad line raises ValueError."""
-        with self.assertRaises(ValueError):
-            self.my_cfg_file.get_dict()
-
-class TestConfigFileEmpty(TestConfigBase):
-    """Test empty line in the "ConfigFile" class."""
-    file_string = "\n\n"
-    def test_empty_line(self):
-        """Test that reading an empty line is fine."""
-        self.my_cfg_file.get_dict()
-
-class TestConfigFileComment(TestConfigBase):
-    """Test comment line in the "ConfigFile" class."""
-    file_string = "  # Hi! \n"
-    def test_comment_line(self):
-        """Test that reading a comment line is fine."""
-        self.my_cfg_file.get_dict()
 
 class TestGitSvnDefFile(TestConfigBase):
     """Test the "TestGitSvnDefFile" class."""
