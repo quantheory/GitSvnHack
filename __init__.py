792b3381bd1ac15449ceb0590fe90bac825ba613
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2013-06-01 21:32:46 -0600

    Add docstring to __init__.py.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""GitSvnHack is a wrapper that extends git-svn.
+
+The goal of this package is to provide a Git interface for Subversion with
+workarounds to handle svn:externals.
+
+The package __init__.py is also the script to be run as git-svnhack.
+
+"""
