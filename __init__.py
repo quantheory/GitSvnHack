9c3c9a375e35e43a7750dfb698156f9791bf6cd5
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2013-06-02 09:46:03 -0600

    Get a basic wrapper for the clone functionality.

--- __init__.py
+++ __init__.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
     # git-svn init options; --no-metadata and --prefix are incompatible
     # with git-svnhack, which needs the metadata and might use --prefix
     # itself in the near future.
+    # Also, add here the git-svnhack options "ignore-revs" and
+    # "config-name".
     init_shortopts = "T:t:b:s"
     init_longopts = [
         "shared=","template="
@@ -33,6 +35,7 @@ if __name__ == "__main__":
         "use-svm-props", "use-svnsync-props",
         "rewrite-root=", "rewrite-uuid=",
         "username=", "no-minimize-url",
+        "ignore-revs", "config-name",
     ]
     # git-svn fetch options
     fetch_shortopts = "r:"
@@ -42,12 +45,70 @@ if __name__ == "__main__":
         "log-window-size=", "use-log-author",
     ]
     if sys.argv[1] == "clone":
-        from GitSvnHack.repository import GitSvnRepo
+        from GitSvnHack.repository import SvnRepo, GitSvnRepo
         shortopts = ""+fetch_shortopts+init_shortopts+gen_shortopts
         longopts = ["preserve-empty-dirs", "placeholder-filename="]
         longopts.extend(gen_longopts)
         longopts.extend(init_longopts)
         longopts.extend(fetch_shortopts)
         opts, args = gnu_getopt(sys.argv[2:], shortopts, longopts)
+        # Here are all the options we need to fill in.
+        opts_d = {
+            "path": args[0],
+            "name": None,
+            "trunk": None,
+            "trunk_tags": None,
+            "ignore_revs": None,
+        }
+        # The git repo's path is used if given, or we grab it from the
+        # end of the svn path.
+        if len(args) > 1:
+            opts_d["git_path"] = args[1]
+        else:
+            opts_d["git_path"] = opts_d["path"].split("/")[-1]
+
+        # Loop over a copy of the options, removing values we handle as we
+        # go.
+        # Note that the first "-t" option has special significance; it is
+        # assumed to hold the trunk tags.
+        for opt in opts[:]:
+            if "trunk" not in opts_d and \
+               opt[0] == "-T" or opt[0] == "--trunk":
+                opts_d["trunk"] = opt[1]
+                opts.remove(opt)
+            elif "trunk_tags" not in opts_d and \
+                 opt[0] == "-t" or opt[0] == "--tags":
+                opts_d["trunk_tags"] = opt[1]
+                opts.remove(opt)
+            elif "ignore_revs" not in opts_d and \
+                 opt[0] == "--ignore-revs":
+                opts_d["ignore_revs"] = opt[1]
+                opts.remove(opt)
+            elif "name" not in opts_d and \
+                 opt[0] == "--config_name":
+                opts_d["name"] = opt[1]
+                opts.remove(opt)
+
+        svn_repo = SvnRepo(
+            name=opts_d["name"]+"_svn",
+            path=opts_d["path"],
+            trunk_head=opts_d["trunk"],
+            trunk_tags=opts_d["trunk_tags"],
+        )
+
+        # Treate --ignore-revs as a comma-separated list.
+        if opts_d["ignore_revs"] is None:
+            ignore_revs = []
+        else:
+            ignore_revs = [int(i) for i in
+                           opts_d["ignore_revs"].split(",")]
+
+        git_svn_repo = GitSvnRepo(
+            name=opts_d["name"],
+            path=opts_d["git_path"],
+            svn_repo=svn_repo,
+            ignore_revs=ignore_revs,
+        )
+
     else:
         os.execvp("git", ["git", "svn"]+sys.argv[1:])
